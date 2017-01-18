8e40c1015bb460e1547fa6ab9b4908fabf4ac783
Vinay <vinayv@thoughtworks.com>
2017-01-18 13:45:12 +0530

    Vinay | #0 | Remove unused AButton component

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,3 @@
-export { AButton } from 'components/medication/AButton.jsx';
 export { AutoComplete } from 'components/medication/AutoComplete.jsx';
 export { MedicationContainer } from 'components/medication/MedicationContainer.jsx';
 
