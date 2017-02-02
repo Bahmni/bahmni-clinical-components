66040fc7ccf7220c25bcc2894c06b1f23bcec255
Shruthip <shruthi.pitta@gmail.com>
2017-02-02 11:54:01 +0530

    Shruthi|Delete unwanted files

--- deployChanges.sh
+++
@@ -1,6 +0,0 @@
-npm run build
-npm pack
-cd ../openmrs-module-bahmniapps/ui
-npm install ../../bahmni-clinical-components/bahmni-clinical-components-0.0.4.tgz
-grunt 'copy:nodeModules'
-cd ../../bahmni-clinical-components
